782c875a1eeff4a61c6795125ffd7eb849f52aaf
Soner Sayakci <s.sayakci@shopware.com>
2021-09-27 19:35:36 +0200

    fix: Show admin components only in html context

--- src/main/kotlin/de/shyim/shopware6/xml/AdminComponentProvider.kt
+++ src/main/kotlin/de/shyim/shopware6/xml/AdminComponentProvider.kt
@@ -3,6 +3,7 @@ package de.shyim.shopware6.xml
 import com.intellij.codeInsight.completion.XmlTagInsertHandler
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.psi.html.HtmlTag
 import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.xml.XmlTag
@@ -14,9 +15,12 @@ import de.shyim.shopware6.index.dict.AdminComponent
 import gnu.trove.THashMap
 import icons.ShopwareToolBoxIcons
 
-
 class AdminComponentProvider : XmlTagNameProvider, XmlElementDescriptorProvider {
     override fun addTagNameVariants(elements: MutableList<LookupElement>, tag: XmlTag, prefix: String?) {
+        if (tag !is HtmlTag) {
+            return
+        }
+
         for (key in FileBasedIndex.getInstance().getAllKeys(AdminComponentIndex.key, tag.project)) {
             val vals = FileBasedIndex.getInstance()
                 .getValues(AdminComponentIndex.key, key, GlobalSearchScope.allScope(tag.project))
