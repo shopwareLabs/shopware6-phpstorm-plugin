697b3c3e4bd553f2fb71d52e1e57998581922240
Soner Sayakci <s.sayakci@shopware.com>
2021-09-29 18:02:20 +0200

    feat: Add ShopwareBundle index

---
+++ src/main/kotlin/de/shyim/shopware6/index/ShopwareBundleIndex.kt
@@ -0,0 +1,91 @@
+package de.shyim.shopware6.index
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor
+import com.intellij.util.indexing.*
+import com.intellij.util.io.EnumeratorStringDescriptor
+import com.intellij.util.io.KeyDescriptor
+import com.jetbrains.php.lang.PhpFileType
+import com.jetbrains.php.lang.psi.elements.PhpClass
+import de.shyim.shopware6.index.dict.ShopwareBundle
+import de.shyim.shopware6.index.externalizer.ObjectStreamDataExternalizer
+import gnu.trove.THashMap
+import java.nio.file.Files
+import kotlin.io.path.Path
+
+class ShopwareBundleIndex: FileBasedIndexExtension<String, ShopwareBundle>() {
+    private val EXTERNALIZER = ObjectStreamDataExternalizer<ShopwareBundle>()
+
+    override fun getName(): ID<String, ShopwareBundle> {
+        return key
+    }
+
+    override fun getVersion(): Int {
+        return 1
+    }
+
+    override fun dependsOnFileContent(): Boolean {
+        return true
+    }
+
+    override fun getIndexer(): DataIndexer<String, ShopwareBundle, FileContent> {
+        return DataIndexer { inputData ->
+            val bundles = THashMap<String, ShopwareBundle>()
+
+            inputData.psiFile.acceptChildren(object: PsiRecursiveElementWalkingVisitor() {
+                override fun visitElement(element: PsiElement) {
+                    if (element is PhpClass && !element.isAbstract && isShopwareBundle(element)) {
+                        bundles[element.name] = ShopwareBundle(element.name, inputData.file.path, getViewDirectory(inputData.file.path))
+                    }
+
+                    super.visitElement(element)
+                }
+            })
+
+            return@DataIndexer bundles
+        }
+    }
+
+    override fun getKeyDescriptor(): KeyDescriptor<String> {
+        return EnumeratorStringDescriptor.INSTANCE
+    }
+
+    override fun getValueExternalizer(): ObjectStreamDataExternalizer<ShopwareBundle> {
+        return EXTERNALIZER
+    }
+
+    companion object {
+        val key = ID.create<String, ShopwareBundle>("de.shyim.shopware6.backend.shopware-bundles")
+    }
+
+    override fun getInputFilter(): FileBasedIndex.InputFilter {
+        return object : DefaultFileTypeSpecificInputFilter(PhpFileType.INSTANCE) {
+        }
+    }
+
+    fun isShopwareBundle(pClass: PhpClass): Boolean {
+        if (pClass.superClasses.size == 0) {
+            return false
+        }
+
+        var found = false
+
+        pClass.superClasses.forEach {
+            if (it.fqn == "\\Shopware\\Core\\Framework\\Bundle" || it.fqn == "\\Shopware\\Core\\Framework\\Plugin") {
+                found = true
+            }
+        }
+
+        return found
+    }
+
+    fun getViewDirectory(folderPath: String): String? {
+        val expectedViewDir = "${folderPath}/Resources/views/storefront"
+
+        if (Files.exists(Path(expectedViewDir))) {
+            return expectedViewDir
+        }
+
+        return null
+    }
+}
---
+++ src/main/kotlin/de/shyim/shopware6/index/dict/ShopwareBundle.kt
@@ -0,0 +1,22 @@
+package de.shyim.shopware6.index.dict
+
+import org.apache.commons.lang.builder.HashCodeBuilder
+import java.io.Serializable
+import java.util.*
+
+class ShopwareBundle(val name: String, val path: String, val viewPath: String?): Serializable {
+    override fun hashCode(): Int {
+        return HashCodeBuilder()
+            .append(this.name)
+            .append(this.path)
+            .append(this.viewPath)
+            .toHashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is ShopwareBundle &&
+                Objects.equals(other.name, this.name) &&
+                Objects.equals(other.path, this.path) &&
+                Objects.equals(other.viewPath, this.viewPath)
+    }
+}
