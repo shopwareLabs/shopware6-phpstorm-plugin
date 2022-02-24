273ceb1bf5edbbc4530b1c3a45bda294b0ea3e11
Soner Sayakci <s.sayakci@shopware.com>
2022-02-24 12:55:28 +0100

    feat: auto suggest commit message for Shopware branch names

---
+++ src/main/kotlin/de/shyim/shopware6/vcs/GitlabCommitMessageProvider.kt
@@ -0,0 +1,30 @@
+package de.shyim.shopware6.vcs
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.changes.LocalChangeList
+import com.intellij.openapi.vcs.changes.ui.CommitMessageProvider
+import git4idea.branch.GitBranchUtil
+import org.apache.commons.lang.StringUtils
+
+class GitlabCommitMessageProvider : CommitMessageProvider {
+    override fun getCommitMessage(forChangelist: LocalChangeList, project: Project): String? {
+        try {
+            val currentBranch = GitBranchUtil.getCurrentRepository(project)?.currentBranch ?: return null
+
+            if (!currentBranch.name.startsWith("next-")) {
+                return null
+            }
+
+            val branchParts = currentBranch.name.split("/")
+
+            if (branchParts.count() == 2) {
+                return "${branchParts[0].uppercase()} - ${StringUtils.capitalize(branchParts[1]).replace("-", " ")}"
+            }
+
+        } catch (e: Exception) {
+            println(e)
+        }
+
+        return null
+    }
+}
