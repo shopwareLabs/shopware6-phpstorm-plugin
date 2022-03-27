63b2f482985f759e631871f03179d5f058dd795e
Soner Sayakci <s.sayakci@shopware.com>
2022-03-27 17:50:56 +0200

    feat: add criteria misused inspection

---
+++ src/main/kotlin/de/shyim/shopware6/inspection/php/CriteriaIdMisused.kt
@@ -0,0 +1,59 @@
+package de.shyim.shopware6.inspection.php
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.jetbrains.php.lang.psi.PhpFile
+import com.jetbrains.php.lang.psi.elements.MethodReference
+import com.jetbrains.php.lang.psi.elements.NewExpression
+import com.jetbrains.php.lang.psi.elements.PhpTypedElement
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression
+import de.shyim.shopware6.inspection.quickfix.php.CriteriaIdMisusedFix
+
+class CriteriaIdMisused: LocalInspectionTool() {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        val file = holder.file
+
+        if (file !is PhpFile) {
+            return super.buildVisitor(holder, isOnTheFly)
+        }
+
+        return object : PsiElementVisitor() {
+            override fun visitElement(element: PsiElement) {
+                if (
+                    element is MethodReference &&
+                    element.nameNode?.text == "addFilter" &&
+                    element.firstChild is PhpTypedElement &&
+                    (element.firstChild as PhpTypedElement).type.toString() == "\\Shopware\\Core\\Framework\\DataAbstractionLayer\\Search\\Criteria" &&
+                    element.parameters.isNotEmpty() &&
+                    element.parameters.first() is NewExpression
+                ) {
+                    val firstArgument = (element.parameters.first() as NewExpression)
+
+                    if (firstArgument.type.toString() != "\\Shopware\\Core\\Framework\\DataAbstractionLayer\\Search\\Filter\\EqualsFilter" && firstArgument.type.toString() != "\\Shopware\\Core\\Framework\\DataAbstractionLayer\\Search\\Filter\\EqualsAnyFilter") {
+                        return
+                    }
+
+                    if (firstArgument.parameters.first() !is StringLiteralExpression) {
+                        return
+                    }
+
+                    if ((firstArgument.parameters.first() as StringLiteralExpression).contents != "id") {
+                        return
+                    }
+
+                    holder.registerProblem(
+                        element as PsiElement,
+                        "Criteria: IDs should be set in the constructor to not execute a additional query internally to find the IDs",
+                        ProblemHighlightType.WARNING,
+                        CriteriaIdMisusedFix()
+                    )
+                }
+
+                super.visitElement(element)
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/de/shyim/shopware6/inspection/quickfix/php/CriteriaIdMisusedFix.kt
@@ -0,0 +1,62 @@
+package de.shyim.shopware6.inspection.quickfix.php
+
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor
+import com.jetbrains.php.lang.PhpFileType
+import com.jetbrains.php.lang.psi.elements.MethodReference
+import com.jetbrains.php.lang.psi.elements.impl.NewExpressionImpl
+
+class CriteriaIdMisusedFix: LocalQuickFix {
+    override fun getFamilyName() = "Move ID filter to Criteria constructor"
+    override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+        if (descriptor.psiElement !is MethodReference) {
+            return
+        }
+
+        val methodRef: MethodReference = descriptor.psiElement as MethodReference
+
+        CommandProcessor.getInstance().executeCommand(project, {
+            val criteriaConstructors: MutableList<NewExpressionImpl> = ArrayList()
+
+            methodRef.parent.parent.acceptChildren(object : PsiRecursiveElementWalkingVisitor() {
+                override fun visitElement(element: PsiElement) {
+                    if (element is NewExpressionImpl && element.classReference !== null && element.classReference!!.fqn == "\\Shopware\\Core\\Framework\\DataAbstractionLayer\\Search\\Criteria") {
+                        criteriaConstructors.add(element)
+                        return
+                    }
+
+                    super.visitElement(element)
+                }
+            })
+
+            if (criteriaConstructors.size == 0) {
+                Messages.showInfoMessage("Cannot find Criteria constructor in current context", "Info")
+                return@executeCommand
+            }
+
+            if (criteriaConstructors.size != 1) {
+                Messages.showInfoMessage("Found multiple Criteria constructor. Cannot modify it", "Info")
+                return@executeCommand
+            }
+
+            val filterArgument: PsiElement = (methodRef.parameters.first() as NewExpressionImpl).parameters[1]
+
+            if ((methodRef.parameters.first() as NewExpressionImpl).classReference?.type.toString() == "\\Shopware\\Core\\Framework\\DataAbstractionLayer\\Search\\Filter\\EqualsFilter") {
+                val newFile = PsiFileFactory.getInstance(project).createFileFromText("test.php", PhpFileType.INSTANCE, "<?php return [1];");
+                newFile.firstChild.children[0].children[0].children[0].children[0].replace(filterArgument)
+
+                criteriaConstructors.first().parameterList?.add(newFile.firstChild.children[0].children[0])
+            } else {
+                criteriaConstructors.first().parameterList?.add(filterArgument)
+            }
+
+            descriptor.psiElement.parent.delete()
+        }, "Changing constructor", null)
+    }
+}
