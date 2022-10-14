335ace6a0af15a2fd0ab21f649c6fe84d61487d8
Soner Sayakci <s.sayakci@shopware.com>
2022-10-14 14:56:30 +0200

    fix: add tests for system config, fixes #86

---
+++ src/test/kotlin/de/shyim/shopware6/test/index/SystemConfigIndexTest.kt
@@ -0,0 +1,25 @@
+package de.shyim.shopware6.test.index
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import de.shyim.shopware6.util.SystemConfigUtil
+
+class SystemConfigIndexTest : BasePlatformTestCase() {
+    override fun setUp() {
+        super.setUp()
+        myFixture.copyDirectoryToProject("src", "src")
+        myFixture.copyFileToProject("composer.json")
+    }
+
+    override fun getTestDataPath(): String {
+        return "src/test/testData/index/SystemConfigIndexTest/"
+    }
+
+    fun testEntityIsIndexed() {
+        val configs = SystemConfigUtil.getAllConfigs(project)
+
+        assertEquals(20, configs.size)
+        assertEquals("SwagFooBar.config", configs[0].namespace)
+        assertEquals("senderAddressFirstName", configs[0].name)
+        assertEquals("customsInformationComment", configs[19].name)
+    }
+}
